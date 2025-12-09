8f403f7c0b1dcff429cf81b5aab25eb80f88a1e7
Mathieu Méa <mathieu.mea@gmail.com>
2025-12-09 08:25:50 -0500

    Update WindsorTransitBusAgencyTools.java

--- agency-parser/src/main/java/org/mtransit/parser/ca_windsor_transit_bus/WindsorTransitBusAgencyTools.java
+++ agency-parser/src/main/java/org/mtransit/parser/ca_windsor_transit_bus/WindsorTransitBusAgencyTools.java
@@ -11,7 +11,7 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MDirection;
 
 import java.util.Arrays;
 import java.util.List;
@@ -156,8 +156,8 @@ public class WindsorTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public List<Integer> getDirectionTypes() {
 		return Arrays.asList(
-				MTrip.HEADSIGN_TYPE_DIRECTION,
-				MTrip.HEADSIGN_TYPE_STRING
+				MDirection.HEADSIGN_TYPE_DIRECTION,
+				MDirection.HEADSIGN_TYPE_STRING
 		);
 	}
 
